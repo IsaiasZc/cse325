305718a93b1b8552c752d78b95eb2c01994b365f
IsaiasZc <coroxerox2@gmail.com>
2026-07-03 20:52:06 -0500

    w01 learning - first commit

---
+++ w01_learning/MyConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿var date = DateTime.Now;
+var daysUntilChristmas = (new DateTime(date.Year, 12, 25) - date).Days;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + date);
+Console.WriteLine($"There are {daysUntilChristmas} days until the next Christmas.");
